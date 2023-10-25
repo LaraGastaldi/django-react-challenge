6b59d42f69f8748e14c65f209ab996f07dbcea60
LaraGastaldi <lara.f.gastaldi@gmail.com>
2023-10-25 11:50:17 -0300

    employee and company migrations added

--- bix_backend/user/models.py
+++ bix_backend/user/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from company.models import Company
 
-# Create your models here.
+class Employee(models.Model):
+    # user = models.oneToOneField(User, on_delete=models.CASCADE)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    email = models.EmailField()
+    name = models.CharField(max_length=100)
