34e687622332e1b760f445905d2d3e9c260f6c42
LaraGastaldi <lara.f.gastaldi@gmail.com>
2023-10-25 17:10:37 -0300

    making employee routing

--- bix_backend/user/models.py
+++ bix_backend/user/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 from company.models import Company
 
+
 class Employee(models.Model):
     # user = models.oneToOneField(User, on_delete=models.CASCADE)
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
     email = models.EmailField()
     name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return {"id": self.id, "email": self.email, "name": self.name, "company": self.company.name}
--- bix_backend/user/views.py
+++ bix_backend/user/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+import json
+from rest_framework.views import APIView
 
-# Create your views here.
+from rest_framework import authentication
+from rest_framework.response import Response
+
+from models import Employee
+
+
+class User(APIView):
+
+    def map_employee(self, employee):
+        return {
+            "email": employee.email,
+                 "id": employee.id,
+                 "name": employee.name,
+                 "company": employee.company.name
+        }
+
+
+    authentication_classes = [authentication.TokenAuthentication]
+    def post(self, request, id):
+        pass
+
+    def put(self, request, id):
+        employee = Employee.objects.get(id=id)
+        for attr, value in json.loads(request.body.decode('utf-8')).values():
+            employee[attr] = value
+        employee.save()
+
+    def get(self):
+        # return Response(Employee.objects.filter(company_id=request.GET.get('company_id')))
+        return Response(map(self.map_employee, Employee.objects.all()))
